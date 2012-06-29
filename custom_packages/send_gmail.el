25a391041ad69d16fb042a2b74bec116bf1ebe2b
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2012-06-29 19:56:35 +0530

    Removed the now unused send_gmail file as the variables are not part of my_customizations (off-repo).

--- custom_packages/send_gmail.el
+++
@@ -1,8 +0,0 @@
-(setq message-send-mail-function 'smtpmail-send-it
-      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "thejaswi@agiliq.com" nil))
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 587
-      smtpmail-local-domain "agiliq.com")
-
-(provide 'send_gmail)
