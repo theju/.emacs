3dbd2fe602d0faecbed16917ff547f96626255d5
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2012-06-29 20:57:13 +0530

    WIP. Broke out the git log functionality out of the idonethis send mail function.

--- custom_packages/idonethis.el
+++ custom_packages/idonethis.el
@@ -1,7 +1,5 @@
-(require 'magit)
-
 (defgroup idonethis nil
-  "A small library to send out git logs to idonethis."
+  "A small library to send mail to idonethis."
   :group 'applications)
 
 (defcustom idonethis-address ""
@@ -21,39 +19,33 @@ Usually something like xyz@team.idonethis.com"
   (set-default symbol (shell-command-to-string "date -d yesterday +%Y-%m-%d")))
 
 (defcustom since-when nil
-  "The date since when to send out the log"
+  "The date since when to send out the git log.
+Not required if not using git or magit."
   :set 'date-since-when
   :group 'idonethis
   :type 'string)
 
-(defcustom summary-buffer "*summary*"
-  "The name of the temporary buffer where the summary generated is stored.
-Shouldn't be required for customization since it is killed at
-the end of the log generation."
-  :group 'idonethis
-  :type 'string)
-
-(defun idonethis-send-log ()
+(defun idonethis-send (address subject &optional buffer)
+  "A function that opens up a mail buffer addressed to the 'idonethis-adress."
   (interactive)
-  (compose-mail idonethis-address idonethis-subject)
-  (let ((since-date since-when)
-	(mail-buffer (current-buffer))
-	(summary-buffer-name summary-buffer))
-    (magit-log nil (concat "--since=" since-date))
-    (with-current-buffer "*magit-log*"
-      (let ((start (lambda ()
-		     (save-excursion
-		       (goto-char (point-min))
-		       (next-line)
-		       (point))))
-	    (end (point-max))
-	    (summary-buffer (get-buffer-create summary-buffer-name)))
-	(append-to-buffer summary-buffer (funcall start) end)))
-    (with-current-buffer summary-buffer-name
-      (reverse-region (point-min) (point-max))
-      (append-to-buffer mail-buffer (point-min) (point-max)))
-    (with-current-buffer mail-buffer
-      (message-send-mail-function))
-    (kill-buffer (get-buffer summary-buffer-name))))
+  (compose-mail address subject ("Body" . buffer)))
+
+(defun idonethis-send-git-log ()
+  "Helper function to send out git logs of a repository."
+  (interactive)
+  (require 'magit)
+  (with-temp-buffer
+    (let ((temp-buffer (current-buffer)))
+      (magit-log nil (concat "--since=" since-when))
+      (with-current-buffer "*magit-log*"
+	(let ((start (lambda ()
+		       (save-excursion
+			 (goto-char (point-min))
+			 (next-line)
+			 (point))))
+	      (end (point-max)))
+	  (append-to-buffer temp-buffer (funcall start) end))))
+    (reverse-region (point-min) (point-max))
+    (idonethis-send idonethis-address idonethis-subject (current-buffer))))
 
 (provide 'idonethis)
